5c4c917318eeb8c6de0900307b36b028ff780a77
Andrusi4ka <Andrusi4ka1985@gmail.com>
2025-07-30 12:23:30 +0200

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.querySelector("#menu-toggle").onclick = () => {
+    document.querySelector('#mobile-menu').classList.toggle('hidden');
+}
+
+
+document.querySelector("#btn-request").onclick = () => {
+    document.querySelector('#modal-request').classList.toggle('hidden');
+}
